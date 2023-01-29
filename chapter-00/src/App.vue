6f5b231c434ec5570ce69dd146a39b72401b7551
Simps <vov4.games@yandex.ru>
2023-01-29 22:17:20 +0200

    added

--- chapter-00/src/App.vue
+++ chapter-00/src/App.vue
@@ -3,30 +3,55 @@
 
     <div class ="card">
 
-      <h2>HELLO MAAILMA {{ now }}</h2>
+      <h2>NEWS NOW {{ now }}</h2>
 
+      
       <!-- помещаем  в шаблон компонент TheHeader -->
       <!--<the-header></the-header> -->
- 
+
+      <!-- помещаем  в шаблон компонент AppNews -->
+      <app-news 
+
+        v-for="item in news"
+        v-bind:key="item.id"
+        v-bind:title="item.title"
+        v-bind:id="item.id"
+        v-bind:isOpen="item.isOpen"
+  
+      ></app-news> 
+
     </div>
 
-    <div class ="card" v-for="item in news" :key="item">
-      <h3>{{ item }}</h3>
-    </div>
-
-
+     
   </div>
   
 </template>
 
 <script>
+ 
+import AppNews from './AppNews.vue';
 
 export default {
   data(){
     return{
       now: new Date().toLocaleDateString(),
-      news:['Uutiset 1', 'UUtiset 2']
-    }
+      news:[{
+        title:'Президент Турции Тайип Эрдоган заявил, что Анкара может принять решение по членству Финляндии в НАТО', 
+        id: 1,
+        isOpen: false   
+        },
+        {
+        title:'Власти США продолжают поддерживать Киев из-за запасов титана',
+        id: 2,
+        isOpen: false
+        } 
+    
+          ]}
+  },
+  components:{
+    // локальное регистрация компонента 
+    'app-news': AppNews
+      
   }
     
 }
---
+++ chapter-00/src/AppNews.vue
@@ -0,0 +1,57 @@
+<!-- это компонент:
+    компонент - это маленькое Vue приложение и его нужно импортировать в main.js потом зарегистрировать 
+    для того чтобы "Vue" знала о нем его нужно зарегистрировать -->
+
+<template>
+    <div class="card" >
+        <h3>{{ title }}</h3>
+        <button class="btm" @click="isOpen = !isOpen">открыть</button>
+        <p v-if="isOpen">В этой статье компонент Vue.js будет использоваться для 
+            добавления функции комментариев на страницу сведений о блоге, 
+            в основном для достижения следующих функций
+        </p>
+
+
+    </div>
+        
+    
+</template>
+    
+    
+<script>
+    
+    export default{
+
+        props:{
+
+            title: {
+                type: String,
+                required: true
+            },
+            id:{
+                type: Number,
+                required: true
+            },
+            /*
+            isOpen: {
+                type: Boolean,
+                required: false,
+                default: false,
+                validator(value){
+                    console.log(value)
+                    return value === true || value === false
+                }
+            }
+            */
+            
+        },
+
+        data(){
+            return{
+              isOpen: false
+            }
+        }
+    }
+    
+    // 39:44
+</script>
