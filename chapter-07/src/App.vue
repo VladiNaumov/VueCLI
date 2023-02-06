6d43d01e8e22d6acccaf3643bab02baea405ec1c
Simps <vov4.games@yandex.ru>
2023-02-07 01:51:36 +0200

    added

--- chapter-07/src/App.vue
+++ chapter-07/src/App.vue
@@ -1,13 +1,34 @@
 <template>
   <div class="container">
-    <div class="card">
+    <!--отменяем обычное поведение html т.е данная страница не будет перегружаться-->
+    <form class="card" @submit.prevent="createPersen">
       <h2>Работа с базой данных</h2>
-    </div>
+
+      <div class="form-control">
+        <label for="name">введите имя</label>
+        <!--трекаем данный imput-->
+        <input type="text" id="name" v-model.trim="name">
+
+      </div>
+        <!--если name ничего не писали т.е name равно пустой строке :disamble = true то кнопка будет выключена-->
+      <button class="btm primary" :disabled="name.length ===0">создать человека</button>
+    </form>
+
   </div>
 </template>
 
 <script>
 export default {
+  data(){
+
+    return{
+      name:''
+    } 
+  }, methods:{
+      createPersen(){
+        // this.name 05.01
+      }
+  }
 }
 </script>
 
