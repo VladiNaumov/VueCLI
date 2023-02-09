6d46f1c3d5c06f328e228ea4f64465e2a5afb5a9
Simps <vov4.games@yandex.ru>
2023-02-09 19:23:08 +0200

    added

--- chapter-07/src/App.vue
+++ chapter-07/src/App.vue
@@ -1,34 +1,111 @@
 <template>
   <div class="container">
-    <!--отменяем обычное поведение html т.е данная страница не будет перегружаться-->
-    <form class="card" @submit.prevent="createPersen">
+    <app-alert :alert="alert" @close="alert = null"></app-alert>
+
+    <form class="card" @submit.prevent="createPerson">
       <h2>Работа с базой данных</h2>
 
       <div class="form-control">
-        <label for="name">введите имя</label>
-        <!--трекаем данный imput-->
+        <label for="name">Введите имя</label>
         <input type="text" id="name" v-model.trim="name">
-
       </div>
-        <!--если name ничего не писали т.е name равно пустой строке :disamble = true то кнопка будет выключена-->
-      <button class="btm primary" :disabled="name.length ===0">создать человека</button>
+
+      <button class="btn primary" :disabled="name.length === 0">Создать человека</button>
     </form>
 
+    <app-loader v-if="loading"></app-loader>
+
+    <app-people-list
+      v-else
+      :people="people"
+      @load="loadPeople"
+      @remove="removePerson"
+    ></app-people-list>
   </div>
 </template>
 
 <script>
-export default {
-  data(){
+import AppPeopleList from './components/AppPeopleList'
+import AppAlert from './components/AppAlert'
+import AppLoader from './components/AppLoader'
+//import axios from 'axios'
 
-    return{
-      name:''
-    } 
-  }, methods:{
-      createPersen(){
-        // this.name 05.01
+export default {
+  data() {
+    return {
+      name: '',
+      people: [],
+      alert: null,
+      loading: false
+    }
+  },
+  mounted() {
+    this.loadPeople()
+  },
+  methods: {
+    async createPerson() {
+      const response = await fetch('https://vue-with-http.firebaseio.com/people.json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          firstName: this.name
+        })
+      })
+
+      const firebaseData = await response.json()
+
+      this.people.push({
+        firstName: this.name,
+        id: firebaseData.name
+      })
+
+      this.name = ''
+    },
+    loadPeople() {
+      this.loading = true
+      setTimeout(async () => {
+        try {
+          const {data} = await axios.get('https://vue-with-http.firebaseio.com/people.json')
+          if (!data) {
+            throw new Error('Список людей пуст')
+          }
+
+          this.people = Object.keys(data).map(key => {
+            return {
+              id: key,
+              ...data[key]
+            }
+          })
+          this.loading = false
+        } catch (e) {
+          this.alert = {
+            type: 'danger',
+            title: 'Ошибка!',
+            text: e.message
+          }
+          this.loading = false
+          console.log(e.message)
+        }
+      }, 1500)
+    },
+    async removePerson(id) {
+      try {
+        const name = this.people.find(person => person.id === id).firstName
+        await axios.delete(`https://vue-with-http.firebaseio.com/people/${id}.json`)
+        this.people = this.people.filter(person => person.id !== id)
+        this.alert = {
+          type: 'primary',
+          title: 'Успешно!',
+          text: `Пользователь с именем "${name}" был удален`
+        }
+      } catch (e) {
+
       }
-  }
+    }
+  },
+  components: {AppPeopleList, AppAlert, AppLoader}
 }
 </script>
 
